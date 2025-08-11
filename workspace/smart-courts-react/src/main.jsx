4c9d7bbac58a056bf46535c71c71d081736de9c3
divyakumarprajapati <divyakumarprajapati1@gmail.com>
2025-08-12 01:40:44 +0530

    scrollfix

--- workspace/smart-courts-react/src/main.jsx
+++ workspace/smart-courts-react/src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './App.css'
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ComputerVision from './pages/ComputerVision.jsx'
-import MatchOrchestration from './pages/MatchOrchestration.jsx'
-import PlayerAnalytics from './pages/PlayerAnalytics.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./App.css";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ComputerVision from "./pages/ComputerVision.jsx";
+import MatchOrchestration from "./pages/MatchOrchestration.jsx";
+import PlayerAnalytics from "./pages/PlayerAnalytics.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop /> {/* This ensures scroll resets on navigation */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/computer-vision" element={<ComputerVision />} />
@@ -18,5 +20,5 @@ createRoot(document.getElementById('root')).render(
         <Route path="/player-analytics" element={<PlayerAnalytics />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
